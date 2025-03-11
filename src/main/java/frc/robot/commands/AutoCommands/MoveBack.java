e8283e87d1e99b6950d77710c97f4506ac22a996
thedoomoo <sebbydooucan2@gmail.com>
2025-03-10 22:27:49 -0400

    2 coral auto crtl + c / crtl + v (not tested)

---
+++ src/main/java/frc/robot/commands/AutoCommands/MoveBack.java
@@ -0,0 +1,60 @@
+package frc.robot.commands.AutoCommands;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.photonVisionConstants;
+import frc.robot.Subsystems.Arm.Arm;
+import frc.robot.Subsystems.Elevator.Elevator;
+import frc.robot.Subsystems.Swerve.SwerveSubsystem;
+
+public class MoveBack extends Command {
+
+    private SwerveSubsystem swerveSubsystem; 
+    private boolean isDone; 
+    private PIDController transController = new PIDController(photonVisionConstants.driveP, photonVisionConstants.driveI, photonVisionConstants.driveD);
+    
+
+
+    public MoveBack(SwerveSubsystem swerveSubsystem) { 
+        this.swerveSubsystem = swerveSubsystem; 
+        addRequirements(swerveSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+        isDone = false; 
+        swerveSubsystem.resetOdometry(new Pose2d());
+    }
+
+    @Override
+    public void execute() {
+
+        double xSpeed = 0; 
+
+        xSpeed = transController.calculate(swerveSubsystem.getPose().getX(), 1);   
+
+        ChassisSpeeds chassisSpeeds = new ChassisSpeeds(-xSpeed, 0, 0);
+        SwerveModuleState[] moduleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(chassisSpeeds);
+        swerveSubsystem.setModuleStates(moduleStates);
+
+        if(swerveSubsystem.getPose().getX() > .5) { 
+            isDone = true; 
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+
+    @Override
+    public boolean isFinished() {
+        return isDone; 
+    }
+    
+}
--- src/main/java/frc/robot/commands/AutoCommands/RedTwoCoral.java
+++ src/main/java/frc/robot/commands/AutoCommands/RedTwoCoral.java
@@ -2,7 +2,6 @@ package frc.robot.commands.AutoCommands;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Subsystems.Arm.Arm;
@@ -12,21 +11,22 @@ import frc.robot.commands.AprilTagAlignmentCommands.AutoAlignToReef;
 import frc.robot.commands.AprilTagAlignmentCommands.AutoAlignToSource;
 import frc.robot.commands.SubsystemAlignmentCommands.Level4Align;
 
-public class BlueTwoCoral extends SequentialCommandGroup {
+public class RedTwoCoral extends SequentialCommandGroup {
 
-    public BlueTwoCoral(SwerveSubsystem swerveSubsystem, Arm arm, Elevator elevator) {
+    public RedTwoCoral(SwerveSubsystem swerveSubsystem, Arm arm, Elevator elevator) {
 
         //does stuff
         addCommands(
             new GrabCoral(elevator, arm),     
             new WaitCommand(.5),
-            new ParallelDeadlineGroup(new AutoAlignToReef(swerveSubsystem, 21, true)), 
-            new MoveRight(swerveSubsystem),
-            new GetToCoralStation(swerveSubsystem, 13),
-            new AutoAlignToSource(swerveSubsystem, 13),
+            new ParallelDeadlineGroup(new AutoAlignToReef(swerveSubsystem, 10, true), new Level4Align(arm, elevator)), 
+            new MoveRight(swerveSubsystem), 
+            new GetToCoralStation(swerveSubsystem, 1),
+            new AutoAlignToSource(swerveSubsystem,1),
             new WaitCommand(2),
             new ParallelCommandGroup(new MoveBack(swerveSubsystem),new GrabCoral(elevator,arm)),
-            new ParallelDeadlineGroup(new AutoAlignToReef(swerveSubsystem, 19, true), new Level4Align(arm, elevator))
+            new ParallelDeadlineGroup(new AutoAlignToReef(swerveSubsystem, 6, true), new Level4Align(arm, elevator))
+            
         );
     }
 }
